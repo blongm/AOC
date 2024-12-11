d40b2692485837d83e362d6884f99acab4aa2cf7
Peter <peterbalta12@gmail.com>
2024-12-11 01:55:44 +0000

    too low :(

--- 2024/day8.cpp
+++ 2024/day8.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <tuple>
 #include <algorithm>
+
 
 using namespace std; 
 
@@ -19,6 +19,40 @@ typedef struct Ant : Node
 
 } Ant;
 
+int width;
+int len;
+vector<Ant> Ants;
+vector<Node> AntiNodes;
+
+auto add_anti_node(Ant A, int x, int y)
+{
+    if (none_of(AntiNodes.begin(), AntiNodes.end(), [A, x, y](Node N){return N.x == x and N.y == y;}))
+        AntiNodes.push_back({x, y});    
+}
+
+auto chk_anti_node(Ant A, Ant Candidate)
+{
+    auto x = Candidate.x;
+    auto y = Candidate.y;
+    auto x_diff = x - A.x;
+    auto y_diff = y - A.y;
+
+    if (none_of(AntiNodes.begin(), AntiNodes.end(), [x, y](Node N){return N.x == x and N.y == y;}))
+    {
+        add_anti_node(A, A.x - x_diff, A.y - y_diff);
+        add_anti_node(A, A.x + 2 * x_diff, A.y + 2 * y_diff);
+    }
+}
+
+auto proc_cell(Ant Candidate)
+{
+    if (Candidate.c != '.') 
+    {
+        for_each(Ants.begin(), Ants.end(), [Candidate](Ant A) {if (Candidate.c == A.c) chk_anti_node(A, Candidate);});
+
+        Ants.push_back(Candidate);
+    }
+}
 
 int main(int argc, char** argv)
 {
@@ -27,46 +61,23 @@ int main(int argc, char** argv)
 
     string line;
 
-    vector<Ant> Ants;
-    vector<Node> AntiNodes;
-
-    for (auto y = 0; getline(file, line); y++)
+    for (auto y = 0; getline(file, line); len = ++y)
     {
-        for (auto x = 0; x < line.size(); x++)
-        {
-            if (line.at(x) != '.')
-            {
-                auto c = line.at(x);
-
-                for (auto A : Ants)
-                {
-                    if (c == A.c)
-                    {
-                        if (find_if(AntiNodes.begin(), AntiNodes.end(), [x, y](Node N){return N.x != x and N.y != y;}))
-                        {
-                            auto x_diff = x - A.x;
-                            auto y_diff = y - A.y;
-                            AntiNodes.push_back({A.x - x_diff, A.y - y_diff});
-                            AntiNodes.push_back({x + x_diff, y + y_diff});
-                        }
-
-                        Ants.push_back({x, y, c});
-                    }
-                }
-            }
-        }
+        width = line.size();
+        
+        for (auto x = 0; x < width; x++)
+            proc_cell({x, y, line.at(x)});
     }
-    //  for every cell
-    //      if attena
-    //          if other other type of attena has been visited
-    //              calc difference 
-    //              calc antinodes if not out of range and not already exists
-    //          add antenna to visited
-    //  get number of antinodes
 
     file.close();
 
-    cout << "Hello, World!\n";
+    int count = 0;
+
+    for (auto AN : AntiNodes)
+        count += AN.x >= 0 and AN.y >= 0 and AN.x < width and AN.y < len;
+    
+    // 370 too low
+    cout << count << '\n';
 
     return 0;
 }
