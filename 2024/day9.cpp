8bd135610e3417c6bb4e767b8b11e355fa5ca03c
Peter <blongmister@gmail.com>
2024-12-11 23:57:05 +0000

    Finished Day9-1, starting p2

--- 2024/day9.cpp
+++ 2024/day9.cpp
@@ -5,17 +5,20 @@
 
 using namespace std; 
 
-auto mov_block(vector<int>& disk, int i)
+auto mov_block(vector<int>& disk, size_t i)
 {
-    if (disk.back() == disk.at(i))
+    if (disk.back() == FREE and i == disk.size() - 1)
     {
         disk.pop_back();
         return;
     }
 
-    while (disk.back() == FREE) disk.pop_back();
+    while (disk.back() == FREE)
+    {
+        disk.pop_back();
+    }
 
-    if (disk.at(i) == FREE)
+    if (i <= disk.size() - 1 and disk.at(i) == FREE)
     {
         disk.at(i) = disk.back();
         disk.pop_back();
@@ -33,13 +36,17 @@ int main(int argc, char** argv)
     for (auto ID = 0; file.get(num); ID++)
     {
         disk.insert(disk.end(), num - '0', ID);
-        
+
         if (file.get(num))
             disk.insert(disk.end(), num - '0', FREE);
+        
     }
 
     file.close();
-    auto checksum = 0;
+
+    auto disk2  = disk;
+
+    long long checksum = 0;
 
     for (size_t i = 0; i < disk.size(); i++)
     {
@@ -48,6 +55,18 @@ int main(int argc, char** argv)
     }
 
     cout << "Checksum: " << checksum << '\n';   
+
+    for (size_t i = disk.size() - 1; i <= 0; i--)
+    {
+    //  if no end position recorded and not free
+    //      record end position
+    //  if end position recorded and free
+    //      record start position
+    //      calc difference
+    //      if free spot exists for the whole file
+    //          move file over
+    //      reset end and start positions
+    }  
 
     return 0;
 }
