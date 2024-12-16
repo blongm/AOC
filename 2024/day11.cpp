354b54b6bf91d8242499602b7f315436f3c7627e
Peter <blongmister@gmail.com>
2024-12-16 11:25:53 +0000

    Working? Still takes a long time to reach 75

--- 2024/day11.cpp
+++ 2024/day11.cpp
@@ -2,7 +2,8 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <map>
+#include <algorithm>
+#include <memory>
 
 using namespace std; 
 
@@ -30,32 +31,86 @@ auto digits(long long num)
     
 }
 
-auto blink(vector<long long>& stones, map<long long, int>& memo)
+
+class Stone
 {
-    for(size_t i = 0; i < stones.size(); i++)
-    {
-        if (stones.at(i) == 0)
+    public:
+
+        long long dat;
+        shared_ptr<Stone> SubStone1;
+        shared_ptr<Stone> SubStone2;
+        Stone(long long dat)
         {
-            stones.at(i) = 1;
-            continue;
+            this->dat = dat;
+            SubStone1 = nullptr;
+            SubStone2 = nullptr;
         }
-        
-        else if (digits(stones.at(i)) % 2 == 0)
+
+        auto calc_sub_stones(vector<shared_ptr<Stone>>& Stones)
         {
-            auto stone_str = to_string(stones.at(i));
-            auto half = stone_str.length() / 2;
-            stones.at(i) = atoll(stone_str.substr(half, half).data());
-            stones.insert(stones.begin() + i, atoll(stone_str.substr(0, half).data()));
-            i++;
-            continue;
-        }
-        else
-        {
-            stones.at(i) *= 2024;
-            continue;
-        }
+            shared_ptr<Stone> left = nullptr;
+            shared_ptr<Stone> right = nullptr;
+
+            if (dat == 0)
+            {
+                right = make_shared<Stone>(1);
+            }
             
-    }    
+            else if (digits(dat) % 2 == 0)
+            {
+                auto stone_str = to_string(dat);
+                auto half = stone_str.length() / 2;
+
+                left = make_shared<Stone>(atoll(stone_str.substr(half, half).data()));
+                right = make_shared<Stone>(atoll(stone_str.substr(0, half).data()));
+
+                if (find_if(Stones.begin(), Stones.end(), [& left](const shared_ptr<Stone>& S){return S->dat == left->dat;}) == Stones.end())
+                {
+                    SubStone1 = left;
+                    Stones.push_back(SubStone1);
+                    
+                }
+                else
+                {
+                    SubStone1 = *find_if(Stones.begin(), Stones.end(), [& left](shared_ptr<Stone>& S){return S->dat == left->dat;});
+                }
+            }
+
+            else
+            {
+                right = make_shared<Stone>(dat * 2024);
+            }
+
+            if (find_if(Stones.begin(), Stones.end(), [& right](const shared_ptr<Stone>& S){return S->dat == right->dat;}) == Stones.end())
+            {
+                SubStone2 = right;
+                Stones.push_back(SubStone2);
+            }
+            else
+            {
+                SubStone2 = *find_if(Stones.begin(), Stones.end(), [&right](const shared_ptr<Stone>& S){return S->dat == right->dat;});
+            }
+        }
+
+        auto size() const
+        {
+            return (SubStone1 != nullptr) + (SubStone2 != nullptr);
+        }
+};
+
+
+auto calc_size(const Stone& Stone, int iter, int maxiter)
+{
+    if (iter == maxiter) return Stone.size();
+
+    auto size = 0;
+    
+    if (Stone.SubStone1 != nullptr)
+        size += calc_size(*Stone.SubStone1, iter + 1, maxiter);
+    
+     size += calc_size(*Stone.SubStone2, iter + 1, maxiter);
+
+     return size;
 }
 
 int main(int argc, char** argv)
@@ -69,27 +124,37 @@ int main(int argc, char** argv)
 
     istringstream stream(line);
 
-    vector<long long> stones;
+    vector<shared_ptr<Stone>> init;
 
-    int stone;
-    while (stream >> stone)  stones.push_back(stone);
-
-    map<long long, int> memo;
-    for (auto i = 0; i < 25; i++)
+    int dat;
+    while (stream >> dat) 
     {
-        cout << "Blink " << i+1 << '\n';
-        blink(stones, memo);
+        init.push_back(make_shared<Stone>(dat));
     }
 
-    cout << "Stones after 25 blinks: " << stones.size() << '\n';
+    vector<shared_ptr<Stone>> Stones = init;
 
-    for (auto i = 25; i < 75; i++)
-     {
-        cout << "Blink " << i+1 << '\n';
-        blink(stones, memo);
+    for (size_t i = 0; i < Stones.size(); i++)
+    {
+        Stones.at(i)->calc_sub_stones(Stones);
+    }
+    
+    auto size = 0;
+
+    for (auto& Stone : init)
+    {
+        size += calc_size(*Stone, 1, 25);
     }
 
-    cout << "Stones after 75 blinks: " << stones.size() << '\n';
+    cout << "Stones after 25 blinks: " << size << '\n';
+
+
+    for (auto& Stone : init)
+    {
+        size += calc_size(*Stone, 1, 55);
+    }
+
+    cout << "Stones after 75 blinks: " << size << '\n';
 
     return 0;
 }
